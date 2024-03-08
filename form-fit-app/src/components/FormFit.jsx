36d865d64e8f61b49dbde7c9771b41d4661c2aaf
MatheusInte <matheus.interaminense.guerra@gmail.com>
2024-03-08 16:08:41 -0300

    feat: estilização FormFit.jsx

--- form-fit-app/src/components/FormFit.jsx
+++ form-fit-app/src/components/FormFit.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom"
+import "../styles/FormFit.css";
 
 const FormFit = ({pessoa}) => {
   return (
     <div className='formfit'>
         <div className='content'>
-            <p>{pessoa.nome}</p>
-            <p>{pessoa.email}</p>
-            <Link to={`/perfil/${pessoa.id}`}>Detalhes</Link>
+            <p className="nome">{pessoa.nome}</p>
+            <div className="dados">
+            <p className="email">{pessoa.email}</p>
+            <p className="numero">{pessoa.telefone}</p>
+            </div>
+            <Link className="link" to={`/perfil/${pessoa.id}`}>
+              <p className="detalhes">Detalhes</p>
+            </Link>
         </div>
     </div>
   )
